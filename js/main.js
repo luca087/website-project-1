60f155891ad8fb2e18867c74c3dd34392f69a1fa
Lucas_L_V <113136493+luca087@users.noreply.github.com>
2023-04-02 09:30:43 -0300

    post

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 var tableData = [];
 
 $(function(){
-    fetch("https://luca087.github.io/website-project-1/DataBaseFiles/Items.json")
+    GetAndSetTable();    
+
+    $("#item-submit-btn").on('click', function(){
+        var name = $("#item-name-input").val();
+        var content = $("#item-content-input").val();
+
+        console.log("name: ", name, "content: ", content);
+        Post(2, name, content);
+    }
+)});
+function GetItems(){
+    var xhr = new XMLHttpRequest();
+
+// open a connection
+xhr.open("GET", "https://luca087.github.io/website-project-1/DataBaseFiles/Items.json", true);
+
+// send the request
+xhr.send();
+
+// handle the response
+xhr.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(this.responseText);
+  }
+}
+}
+function GetAndSetTable(){
+  fetch("https://luca087.github.io/website-project-1/DataBaseFiles/Items.json")
     .then(function (response) {
         return response.json();
       })
@@ -24,27 +51,11 @@ $(function(){
         $("#item-table-body").append(tableHtml);
       }
     });
-
-    $("#item-submit-btn").on('click', function(){
-        var name = $("#item-name-input").val();
-        var content = $("#item-content-input").val();
-
-        console.log("name: ", name, "content: ", content);
-    }
-)});
-function GetItems(){
-    var xhr = new XMLHttpRequest();
-
-// open a connection
-xhr.open("GET", "https://luca087.github.io/website-project-1/DataBaseFiles/Items.json", true);
-
-// send the request
-xhr.send();
-
-// handle the response
-xhr.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText);
-  }
 }
-}
+function Post(id, name, content){
+  tableData.push({
+    ID:id,
+    Name:name,
+    Content:content});
+  $.post("../DataBaseFiles/Items.json", tableData);
+};
