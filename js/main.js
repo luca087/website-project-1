938d1da15eede51da56693f838bb09479f6e45a5
Lucas_L_V <113136493+luca087@users.noreply.github.com>
2023-04-02 16:21:34 +0000

    a

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
-import { createClient } from '../node_modules/@supabase/supabase-js/dist/module/index.js';
+const {createClient} = supabase;
+
 const supabaseUrl = 'https://xvohwimfsjsqkzwmfxse.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
